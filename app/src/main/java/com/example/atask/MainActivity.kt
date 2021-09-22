413877cc6950c448d7987e9c609771ba9292d90b
Iskandar <iskandar200213@gmail.com>
2021-09-22 15:30:47 +0300

    added dagger-hilt

--- app/src/main/java/com/example/atask/MainActivity.kt
+++ app/src/main/java/com/example/atask/MainActivity.kt
@@ -9,7 +9,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.atask.ui.theme.ATaskTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
